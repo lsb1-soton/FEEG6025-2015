0efe53e6510347c11c553254d831fe8a36fb598f
lsb1-soton <lsb1@soton.ac.uk>
2015-10-21 15:05:54 +0100

    commitment

--- clc.R
+++ clc.R
@@ -3,6 +3,7 @@
 # definitely works in RStudio on Windows 7 64bit
 # should work on other systems too
 # see http://stackoverflow.com/questions/14260340/function-to-clear-the-console-in-r
+# Need to add "source('/path/to/clc.R')" to .Rprofile in order to load function at startup
 clc<-function(){
   cat("\014")
   }
